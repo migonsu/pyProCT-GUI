6a534660471569af03dbc1523234dbbbf618f7ff
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-02-12 14:33:27 +0100

    First commit with the code from pyProClust

---
+++ gui/web/js/definitions/query_and_criteria_definitions.js
@@ -0,0 +1,12 @@
+var query_types = ["NumClusters", "NumClusteredElems", "MeanClusterSize",
+                   "PercentInTop4", "PercentInTop", "ClustersTo90", "Cohesion","Separation",
+                   "NoiseLevel", "MirrorCohesion", "MinimumMeanSeparation",
+                   "Silhouette", "CythonMirrorCohesion", "CythonMinimumMeanSeparation",
+                   "CythonSilhouette", "RatioCut", "NCut",
+                   "CythonNormNCut", "NormNCut", "MinMaxCut",
+                   "PCAanalysis", "Details"]
+
+var criteria_types = ["MeanClusterSize", "CythonMirrorCohesion", "CythonMinimumMeanSeparation",
+                       "CythonSilhouette", "RatioCut", "NCut",
+                       "CythonNormNCut", "MinMaxCut", "PCAanalysis"]
+            
