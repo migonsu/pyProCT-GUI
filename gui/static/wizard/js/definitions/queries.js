d11b9bd82f1314a1b70fa2650f36da97645a7786
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-06-12 16:49:19 +0200

    Added Calinski-Harabasz index to queries.

--- gui/static/wizard/js/definitions/queries.js
+++ gui/static/wizard/js/definitions/queries.js
@@ -2,15 +2,15 @@ var QUERIES = (function(){
 	var query_types = ["NumClusters", "NumClusteredElems", "MeanClusterSize",
 	                   "PercentInTop4", "PercentInTop", "ClustersTo90", "Cohesion","Separation",
 	                   "NoiseLevel", "MirrorCohesion", "MinimumMeanSeparation",
-	                   "Silhouette", "CythonMirrorCohesion", "CythonMinimumMeanSeparation",
+	                   "Silhouette", "Calinski-Harabasz", "CythonMirrorCohesion", "CythonMinimumMeanSeparation",
 	                   "CythonSilhouette", "RatioCut", "NCut",
 	                   "CythonNormNCut", "NormNCut", "MinMaxCut",
 	                   "PCAanalysis", "Details"];
 	
 	var criteria_types = ["MeanClusterSize", "CythonMirrorCohesion", "Separation",
-	                      "CythonMinimumMeanSeparation",
+	                      "CythonMinimumMeanSeparation", "Calinski-Harabasz",
 	                      "CythonSilhouette", "RatioCut", "NCut",
-	                      "CythonNormNCut", "MinMaxCut", "PCAanalysis"];
+	                      "CythonNormNCut", "MinMaxCut", "PCAanalysis" ];
 
 	return {
 		query_types:query_types,
