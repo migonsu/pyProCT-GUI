995270b7c5f48d707d503c77cb1f210a3d9fd343
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-06-12 17:56:22 +0200

    added Dunn to queries

--- gui/static/wizard/js/definitions/queries.js
+++ gui/static/wizard/js/definitions/queries.js
@@ -2,13 +2,13 @@ var QUERIES = (function(){
 	var query_types = ["NumClusters", "NumClusteredElems", "MeanClusterSize",
 	                   "PercentInTop4", "PercentInTop", "ClustersTo90", "Cohesion","Separation",
 	                   "NoiseLevel", "MirrorCohesion", "MinimumMeanSeparation",
-	                   "Silhouette", "Calinski-Harabasz", "CythonMirrorCohesion", "CythonMinimumMeanSeparation",
+	                   "Silhouette", "Calinski-Harabasz", "Dunn", "CythonMirrorCohesion", "CythonMinimumMeanSeparation",
 	                   "CythonSilhouette", "RatioCut", "NCut",
 	                   "CythonNormNCut", "NormNCut", "MinMaxCut",
 	                   "PCAanalysis", "Details"];
 	
 	var criteria_types = ["MeanClusterSize", "CythonMirrorCohesion", "Separation",
-	                      "CythonMinimumMeanSeparation", "Calinski-Harabasz",
+	                      "CythonMinimumMeanSeparation", "Calinski-Harabasz", "Dunn",
 	                      "CythonSilhouette", "RatioCut", "NCut",
 	                      "CythonNormNCut", "MinMaxCut", "PCAanalysis" ];
 
