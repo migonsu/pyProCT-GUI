c7966483941fead8dddf2d8219e1004026b389fb
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-06-13 12:25:02 +0200

    Added Davies-Bouldin to queries

--- gui/static/wizard/js/definitions/queries.js
+++ gui/static/wizard/js/definitions/queries.js
@@ -2,13 +2,15 @@ var QUERIES = (function(){
 	var query_types = ["NumClusters", "NumClusteredElems", "MeanClusterSize",
 	                   "PercentInTop4", "PercentInTop", "ClustersTo90", "Cohesion","Separation",
 	                   "NoiseLevel", "MirrorCohesion", "MinimumMeanSeparation",
-	                   "Silhouette", "Calinski-Harabasz", "Dunn", "CythonMirrorCohesion", "CythonMinimumMeanSeparation",
+	                   "Silhouette", "Calinski-Harabasz", "Dunn", "Davies-Bouldin",
+	                   "CythonMirrorCohesion", "CythonMinimumMeanSeparation",
 	                   "CythonSilhouette", "RatioCut", "NCut",
 	                   "CythonNormNCut", "NormNCut", "MinMaxCut",
 	                   "PCAanalysis", "Details"];
 	
 	var criteria_types = ["MeanClusterSize", "CythonMirrorCohesion", "Separation",
-	                      "CythonMinimumMeanSeparation", "Calinski-Harabasz", "Dunn",
+	                      "CythonMinimumMeanSeparation", 
+	                      "Calinski-Harabasz", "Dunn",, "Davies-Bouldin",
 	                      "CythonSilhouette", "RatioCut", "NCut",
 	                      "CythonNormNCut", "MinMaxCut", "PCAanalysis" ];
 
