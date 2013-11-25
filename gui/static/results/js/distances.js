0b711c320ea46c5bf9d9195fa4f09a2a7de48388
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-11-25 18:01:35 +0100

    Distances tab started.

--- gui/static/results/js/distances.js
+++ gui/static/results/js/distances.js
@@ -1,80 +1,107 @@
 
+var DISTANCES = (function(){
 
-function render(){
-	// From http://fhtr.org/BasicsOfThreeJS/#37
-	var renderer = new THREE.WebGLRenderer({antialias: true});
-	
-//	renderer.setSize(document.body.clientWidth, 
-//	                       document.body.clientHeight);
-	renderer.setSize(500,500);
-	
-	// Add to the page (renderer.domElement is the canvas)
-	document.body.appendChild(renderer.domElement);
-	
-	// Clear to light grey
-	renderer.setClearColor(0xEEEEEE, 1.0);
-	renderer.clear();
-	
-	// Create scene and camera
-	var camera = new THREE.PerspectiveCamera(45, /*width/height*/1, 1, 10000);
-	camera.position.z = 300;
-	camera.position.x = 0;
-	camera.position.y = 75;
-	var scene = new THREE.Scene();
-	//scene.fog = new THREE.FogExp2(0xFFFFFF, 0.0035);
-	
-	// Add a light 
-	var light = new THREE.SpotLight();
-	light.position.set( 170, 330, -160 );
-	scene.add(light);
-	// enable shadows on the renderer
-	renderer.shadowMapEnabled = true;
-  // enable shadows for a light
-	light.castShadow = true;
+	var create_tab = function(data){
+		var centers_path = "";
+		var all_files = data.files;
 
-	// Create the scatter plot object
-	var scatterPlot = new THREE.Object3D();
-	scene.add(scatterPlot);
-    scatterPlot.rotation.y = 0.5;
-    
-	var mat = new THREE.ParticleBasicMaterial({
-		vertexColors: true, 
-		size: 1.5
-		});
-	
-	var pointCount = 10000;
-	var pointGeo = new THREE.Geometry();
-	for (var i=0; i<pointCount; i++) {
-		var x = Math.random() * 100 - 50;
-		var y = x*0.8+Math.random() * 20 - 10;
-		var z = x*0.7+Math.random() * 30 - 15;
-		pointGeo.vertices.push(new THREE.Vertex(new THREE.Vector3(x,y,z)));	
-		pointGeo.colors.push(new THREE.Color().setRGB(0.5,0,0));
+		//Look for the centers distance file
+		for(var i = 0; i< all_files.length; i++){
+			if (all_files[i].description == "Centers of the selection used to calculate distances"){
+				centers_path = all_files[i].path;
+			}
+		}
+
+		if( centers_path !== "" ){
+			// Add the tab
+			$("<li><a href='#centers-tab'>Selection Centers</a></li>").insertAfter("#clusters_tab");
+
+			// handle data
+			data.cluster_centers = JSON.parse(COMM.synchronous.load_external_text_resource(centers_path));
+
+			$("#distances-tab").append($(render()));
+
+		}
+	};
+
+	function render(){
+		// From http://fhtr.org/BasicsOfThreeJS/#37
+		var renderer = new THREE.WebGLRenderer({antialias: true});
+
+// 		renderer.setSize(document.body.clientWidth,
+	//	                       document.body.clientHeight);
+		renderer.setSize(500,500);
+
+		renderer.setClearColor(0xEEEEEE, 1.0);
+		renderer.clear();
+
+		// Create scene and camera
+		var camera = new THREE.PerspectiveCamera(45, /*width/height*/1, 1, 10000);
+		camera.position.z = 300;
+		camera.position.x = 0;
+		camera.position.y = 75;
+		var scene = new THREE.Scene();
+		//scene.fog = new THREE.FogExp2(0xFFFFFF, 0.0035);
+
+		// Add a light
+// 		var light = new THREE.SpotLight();
+// 		light.position.set( 170, 330, -160 );
+// 		scene.add(light);
+		// enable shadows on the renderer
+// 		renderer.shadowMapEnabled = true;
+	  // enable shadows for a light
+// 		light.castShadow = true;
+
+		// Create the scatter plot object
+		var scatterPlot = new THREE.Object3D();
+		scene.add(scatterPlot);
+	    scatterPlot.rotation.y = 0.5;
+
+		var mat = new THREE.ParticleBasicMaterial({
+			vertexColors: true,
+			size: 1.5
+			});
+
+		var pointCount = 10000;
+		var pointGeo = new THREE.Geometry();
+		for (var i=0; i<pointCount; i++) {
+			var x = Math.random() * 100 - 50;
+			var y = x*0.8+Math.random() * 20 - 10;
+			var z = x*0.7+Math.random() * 30 - 15;
+			pointGeo.vertices.push(new THREE.Vertex(new THREE.Vector3(x,y,z)));
+			pointGeo.colors.push(new THREE.Color().setRGB(0.5,0,0));
+		}
+		var points = new THREE.ParticleSystem(pointGeo, mat);
+		scatterPlot.add(points);
+
+		renderer.render(scene, camera);
+
+
+		var down = false;
+		var sx = 0, sy = 0;
+		window.onmousedown = function (ev){
+			down = true; sx = ev.clientX; sy = ev.clientY;
+			console.log("down");
+		};
+		window.onmouseup = function(){ down = false; };
+		window.onmousemove = function(ev) {
+			if (down) {
+				var dx = ev.clientX - sx;
+				var dy = ev.clientY - sy;
+				scatterPlot.rotation.y += dx*0.01;
+				camera.position.y += dy;
+				sx += dx;
+				sy += dy;
+				renderer.clear();
+				camera.lookAt(scene.position);
+				renderer.render(scene, camera);
+			}
+		};
+		return renderer.domElement;
 	}
-	var points = new THREE.ParticleSystem(pointGeo, mat);
-	scatterPlot.add(points);
-	
-	renderer.render(scene, camera);
-	
-	
-    var down = false;
-    var sx = 0, sy = 0;
-    window.onmousedown = function (ev){
-      down = true; sx = ev.clientX; sy = ev.clientY;
-      console.log("down")
-    };
-    window.onmouseup = function(){ down = false; };
-    window.onmousemove = function(ev) {
-      if (down) {
-        var dx = ev.clientX - sx;
-        var dy = ev.clientY - sy;
-        scatterPlot.rotation.y += dx*0.01;
-        camera.position.y += dy;
-        sx += dx;
-        sy += dy;
-        renderer.clear();
-        camera.lookAt(scene.position);
-        renderer.render(scene, camera);
-      }
-    }
-}
+
+	return {
+				create_tab:create_tab
+			};
+
+}());
--- gui/static/results/js/main.js
+++ gui/static/results/js/main.js
@@ -12,9 +12,9 @@ function process_result_data(data){
 	data['total_number_of_clusterings'] = Object.keys(all_clusterings).length;
 	data['number_of_accepted_clusterings'] = Object.keys(data["selected"]).length;
 	data['number_of_rejected_clusterings'] = Object.keys(data["not_selected"]).length;
-	
+
 	var tmp_data = {};
-	
+
 	for (clustering_id in all_clusterings){
 		var clustering_definition = all_clusterings[clustering_id];
 		var type = clustering_definition["type"];
@@ -31,34 +31,34 @@ function process_result_data(data){
 		data['number_of_clusterings_by_type'].push({"number_of_clusterings": tmp_data[clustering_type],
 															"type":clustering_type});
 	}
-	
+
 	data["accepted"] = [];
 	var accepted_ids = [];
 	for (var clustering_id in data["selected"]){
 		accepted_ids.push({id:clustering_id});
 		var clustering_definition = data["selected"][clustering_id];
-		data["accepted"].push({ 
+		data["accepted"].push({
 			id:clustering_id,
 			type:clustering_definition["type"],
 			parameters:JSON.stringify(clustering_definition["parameters"])
 		});
 	}
-	
+
 	data["rejected"] = [];
 	for (var clustering_id in data["not_selected"]){
 		var clustering_definition = data["not_selected"][clustering_id];
-		data["rejected"].push({ 
+		data["rejected"].push({
 			id:clustering_id,
 			type:clustering_definition["type"],
 			parameters:JSON.stringify(clustering_definition["parameters"]),
 			reasons: clustering_definition["reasons"]
 		});
 	}
-	
+
 	EVALUATION.process_data(data, accepted_ids);
-	
+
 	//process_cluster_data(data);
-	
+
 	return data;
 }
 
@@ -71,7 +71,7 @@ function generate_all_tabs_contents(data){
 	$("#summary-tab").html(template(data));
 	$("#expandList").button();
 	$("#collapseList").button();
-	
+
 	var files_template = COMM.synchronous.load_text_resource("results/templates/files.template");
 	template = Handlebars.compile(files_template);
 	$("#files-tab").html(template(data));
@@ -81,12 +81,13 @@ function generate_all_tabs_contents(data){
 	});
 
 	EVALUATION.create_tab("evaluation-tab",data);
-	
+
 	CLUSTERS.create_tab(data);
-	
+
 	DISPLACEMENTS.create_tab(data);
-	
+
+	DISTANCES.create_tab(data);
+
 	$("#tabs").tabs();
-	
-	
+
 }
